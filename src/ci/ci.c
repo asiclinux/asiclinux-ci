13775e007b56c4ef750adbdef2fac0342bb4e2ee
allexanderbergmans <allexander.bergmans@student.elisa.be>
2026-06-10 16:28:49 +0200

    * Copyright etc

--- src/ci/ci.c
+++ src/ci/ci.c
@@ -24,6 +24,7 @@
 int ci_run(void) {
     printf("CI run started!\n");
     
+    // Construct a server.
     struct Server server = server_Constructor(AF_INET, 80, SOCK_STREAM, 0, 10, INADDR_ANY, launch);
     server.launch(&server);
     
